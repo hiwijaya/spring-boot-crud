419cdb8992c387fb2686a6dfb04a61d1aa2eeb93
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-06-08 17:53:02 +0700

    styling customer page

--- src/main/java/com/hiwijaya/crud/controller/WebController.java
+++ src/main/java/com/hiwijaya/crud/controller/WebController.java
@@ -1,15 +1,24 @@
 package com.hiwijaya.crud.controller;
 
+import com.hiwijaya.crud.entity.Customer;
+import com.hiwijaya.crud.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * @author Happy Indra Wijaya
  */
 @Controller
 public class WebController {
+
+    @Autowired
+    private CustomerService customerService;
+
 
     // inject via application.properties
     @Value("${welcome.message}")
@@ -20,6 +29,15 @@ public class WebController {
         model.addAttribute("message", message);
 
         return "index";
+    }
+
+    @GetMapping("/customer")
+    public String customer(Model model){
+        List<Customer> customers = customerService.getAll();
+        System.out.println("PANJANG CUSTOMERSSSS: " + customers.size());
+        model.addAttribute("customers", customerService.getAll());
+
+        return "customer";
     }
 
 
