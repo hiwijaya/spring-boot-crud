166f2d7455665a6fca9344de60d1e4721253f0d3
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-06-09 17:49:23 +0700

    creating customer features

--- src/main/java/com/hiwijaya/crud/controller/CustomerController.java
+++ src/main/java/com/hiwijaya/crud/controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -14,7 +15,7 @@ import java.util.List;
  * @author Happy Indra Wijaya
  */
 @Controller
-public class WebController {
+public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
@@ -38,6 +39,24 @@ public class WebController {
         model.addAttribute("customers", customerService.getAll());
 
         return "customer";
+    }
+
+    @GetMapping("/customer/add")
+    public String addCustomer(Model model){
+        model.addAttribute("customer", new Customer());
+        model.addAttribute("editMode", false);
+
+        return "customer-form";
+    }
+
+    @GetMapping("/customer/edit/{id}")
+    public String editCustomer(@PathVariable("id") Integer id, Model model){
+
+        Customer customer = customerService.getCustomerById(id);
+        model.addAttribute("customer", customer);
+        model.addAttribute("editMode", true);
+
+        return "customer-form";
     }
 
 
